0726026107cdca6d380bd3ee5a59ec272380a40a
Sevrim <68111172+Sevrim@users.noreply.github.com>
2020-08-12 10:06:09 +0200

    kommentar abgeändert

--- src/de/awacademy/supermarkt/KasseTest.java
+++ src/de/awacademy/supermarkt/KasseTest.java
@@ -10,7 +10,7 @@ public class KasseTest {
 	@Test
 	public void scanZweiProdukte() {
 		
-		//whats poppin ? & lets fetz
+		//whats poppin ? Hallo Jana, wie geht es dir ?
 		
 		// 1. Testdaten vorbereiten
 		Produkt vanille = new Produkt("Vanille", 299);
